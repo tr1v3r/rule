adc550ec79c2654ce63cc93da20e934c2737849e
r1v3r <churiver@outlook.com>
2023-04-25 17:01:56 +0800

    feat: init routers

--- biz/handler/rule/api.go
+++ biz/handler/rule/api.go
@@ -1,1 +1,52 @@
 package rule
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/riverchu/rule/biz/handler"
+)
+
+func RegisterApi(r *gin.RouterGroup) {
+	// return rules
+	rule := r.Group("rule")
+	{
+		// ping pong
+		rule.GET("ping", handler.Ping)
+
+		// return node rule
+		rule.GET("")
+		// return root template
+		rule.GET("template")
+		// return info about node
+		rule.GET("info")
+
+		// modify rule
+		m := rule.Group("mod")
+		{
+			// update node rule
+			m.POST("")
+			// create or delete rule node
+			m.POST("node")
+			// replace template
+			m.POST("template")
+			// check operates on node
+			m.POST("check")
+		}
+	}
+
+	// list nodes
+	list := r.Group("list")
+	{
+		// list all rule tree name
+		list.GET("name")
+		// list all nodes
+		list.GET("node")
+	}
+
+	// configure about rule enginee
+	config := r.Group("config")
+	{
+		// return config
+		config.GET("")
+	}
+}
--- router.go
+++ router.go
@@ -4,10 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/riverchu/rule/biz/handler"
+	"github.com/riverchu/rule/biz/handler/rule"
 )
 
 func register(r *gin.Engine) *gin.Engine {
 	r.GET("ping", handler.Ping)
+
+	apiV1 := r.Group("api/v1")
+	{
+		rule.RegisterApi(apiV1)
+	}
 
 	return r
 }
