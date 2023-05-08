9e161c1b60f8e0ecddf276f61731d55c09e04da8
r1v3r <churiver@outlook.com>
2023-05-08 19:59:01 +0800

    style: optimize

--- biz/service/rule/engine.go
+++ biz/service/rule/engine.go
@@ -1,42 +1,32 @@
 package rule
 
-import (
-	"sort"
+import "sync"
 
-	"github.com/riverchu/rule/biz/service/driver"
-)
-
+// Forest rule forest
 type Forest struct {
-	_ map[string]*Tree
+	mu sync.RWMutex
+	m  map[string]*Tree
+
+	builders []func() (name string, tree *Tree)
 }
 
-// Rule raw rule
-type Rule struct {
-	Path      string
-	Operators []driver.Operator
+// Build
+func (f *Forest) Build() {
+	for _, builder := range f.builders {
+		f.Set(builder())
+	}
 }
 
-// RuleArray rules array
-type RuleArray []*Rule
-
-func (r RuleArray) Len() int      { return len(r) }
-func (r RuleArray) Swap(i, j int) { r[i], r[j] = r[j], r[i] }
-
-type ruleSorter struct {
-	RuleArray
-	by By
+// Get get rule tree by name
+func (f *Forest) Get(name string) *Tree {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	return f.m[name]
 }
 
-// Less is part of sort.Interface.
-func (s *ruleSorter) Less(i, j int) bool { return s.by(s.RuleArray[i], s.RuleArray[j]) }
-
-// By is the type of a "less" function that defines the ordering of its Rules arguments.
-type By func(x, y *Rule) bool
-
-// Sort is a method on the function type, By, that sorts the argument slice according to the function.
-func (by By) Sort(rules []*Rule) {
-	sort.Sort(&ruleSorter{
-		RuleArray: rules,
-		by:        by, // The Sort method's receiver is the function (closure) that defines the sort order.
-	})
+// Set set rule tree
+func (f *Forest) Set(name string, tree *Tree) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	f.m[name] = tree
 }
