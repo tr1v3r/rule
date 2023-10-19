d715e34b827f9ac76a74ccef64b4c29388204441
tr1v3r <churiver@outlook.com>
2023-10-19 16:19:33 +0800

    feat: implement Lazy Tree build method

--- factory.go
+++ factory.go
@@ -18,26 +18,47 @@ func NewJSONTree[R Rule](name, template string, rules ...R) (Tree, error) {
 	return NewTree(driver.NewJSONDriver(), name, template, rules...)
 }
 
+// NewLazyJSONTree build a lazy json rule tree
+func NewLazyJSONTree[R Rule](name, template string, rules ...R) (Tree, error) {
+	return NewLazyTree(driver.NewJSONDriver(), name, template, rules...)
+}
+
 // NewYAMLTree build a yaml rule tree
 func NewYAMLTree[R Rule](name, template string, rules ...R) (Tree, error) {
 	return NewTree(driver.NewYAMLDriver(), name, template, rules...)
 }
 
-// NewTree build a rule tree.
-func NewTree[R Rule](diver driver.Driver, name, template string, rules ...R) (Tree, error) {
-	tree := &tree[R]{
-		name: name,
+// NewLazyYAMLTree build a yaml rule tree
+func NewLazyYAMLTree[R Rule](name, template string, rules ...R) (Tree, error) {
+	return NewLazyTree(driver.NewYAMLDriver(), name, template, rules...)
+}
 
-		rule:     []byte(template),
-		driver:   diver,
-		children: make(map[string]Tree),
-	}
+// NewTree build a rule tree.
+func NewTree[R Rule](driver driver.Driver, name, template string, rules ...R) (Tree, error) {
+	tree := newTree(driver, name, template, rules)
 	if err := tree.build(rules...); err != nil {
 		return nil, err
 	}
 	return tree, nil
 }
 
-func NewRule(path string, Processors ...driver.Processor) Rule {
-	return &rule{path, Processors}
+// NewLazyTree build a lazy rule tree.
+func NewLazyTree[R Rule](driver driver.Driver, name, template string, rules ...R) (Tree, error) {
+	tree := newTree(driver, name, template, rules).lazy()
+	if err := tree.build(rules...); err != nil {
+		return nil, err
+	}
+	return tree, nil
 }
+
+func newTree[R Rule](diver driver.Driver, name, template string, rules []R) *tree[R] {
+	return (&tree[R]{
+		name: name,
+
+		rule:     []byte(template),
+		driver:   diver,
+		children: make(map[string]Tree),
+	})
+}
+
+func NewRule(path string, Processors ...driver.Processor) Rule { return &rule{path, Processors} }
